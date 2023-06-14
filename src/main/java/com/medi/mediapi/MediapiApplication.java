be6a7d43b17693dae165d5f88da8cc307e8de61f
chcho <chcho@medivalue.co.kr>
2023-06-14 14:17:31 +0900

    스케쥴러 초기 커밋

--- src/main/java/com/medi/mediapi/MediapiApplication.java
+++ src/main/java/com/medi/mediapi/MediapiApplication.java
@@ -1,13 +1,20 @@
 package com.medi.mediapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class MediapiApplication {
+public class MediapiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MediapiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
